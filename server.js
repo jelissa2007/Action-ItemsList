f91b74ab77b5d2f67eca7d25a86b5e7a81b80675
jelissa2007 <jelissa2007@gmail.com>
2021-10-14 20:24:46 -0400

    added files

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ let express = require('express');
 
 let app = express();
 
+app.use(express.urlencoded({extended: false}));
+
 app.get('/', function(req, res){
     // res.send("Hello, welcome to my Action Item List!")
     res.sendFile('todoApp.html', {root: __dirname})
@@ -9,4 +11,14 @@ app.get('/', function(req, res){
     
 
 });
+
+// app.post(a, b)
+
+app.post('/create-item', function(req, res) {
+    console.log(req.body.item)
+
+    res.send('thanks for submiting you rock!')
+
+});
+
 app.listen(3000);
